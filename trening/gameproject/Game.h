dc6987cb563bc4930750107cc0559129d13dcb53
Jakub7374 <jakub.kryger2002@gmail.com>
2022-12-27 14:16:59 +0100

    Ćwiczenia z klasą Game, wskaźnikiem *window i funkcjami

---
+++ trening/gameproject/Game.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include<iostream>
+//#include<vector>
+//#include<ctime>
+//#include<sstream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+/*
+	Class that acts as the game engine.
+	Wrapper class.
+*/
+
+class Game
+{
+private:
+	//Variables
+	//Window
+	sf::RenderWindow* window;
+	sf::VideoMode videoMode;
+	sf::Event ev;
+
+	
+
+	//Private functions
+	void initVariables();
+	void initWindow();
+
+
+public:
+	//Constructors / Destructors
+	Game();
+	virtual ~Game();
+
+	//Accessors
+	const bool running() const;
+
+	//Functions
+
+	void pollEvents();
+
+	void update();
+
+	void render();
+};
